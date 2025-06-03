edc1ac986028a48ab7b4c7d5daae381e658261c4
suryaNoble <suryamani11121@gmail.com>
2025-06-03 09:22:45 +0530

    dashboard portion completed

--- app/routes.ts
+++ app/routes.ts
@@ -1,4 +1,9 @@
-import { type RouteConfig, layout, route } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  layout,
+  route,
+} from "@react-router/dev/routes";
 
 export default [
   route("sign-in", "routes/root/sign-in.tsx"),
@@ -12,4 +17,6 @@ export default [
   ]),
   route("api/create-trip", "routes/api/create-trip.ts"),
   route("trips/:tripId", "routes/admin/trip-detail.tsx"),
+
+  layout("routes/root/home-layout.tsx", [index("routes/root/HomePage.tsx")]),
 ] satisfies RouteConfig;
