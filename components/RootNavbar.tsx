ceeffffe48b75bf8bd37481ae51997f4e95f3551
suryaNoble <suryamani11121@gmail.com>
2025-06-04 21:55:54 +0530

    final commit

--- components/RootNavbar.tsx
+++ components/RootNavbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Link,
   useLoaderData,
@@ -14,6 +14,11 @@ const RootNavbar = () => {
   const location = useLocation();
   const params = useParams();
   const user = useLoaderData();
+  // console.log("user data:", user);
+  // console.log(user.status);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading2, setIsLoading2] = useState(false);
+  const [isLoading3, setIsLoading3] = useState(false);
 
   const handleLogout = async () => {
     await logoutUser();
@@ -26,7 +31,7 @@ const RootNavbar = () => {
         location.pathname === `/travel/${params.tripId}`
           ? "bg-white"
           : "glassmorphism",
-        "w-full fixed z-50"
+        "w-full fixed z-50 "
       )}
     >
       <header className="root-nav wrapper">
@@ -38,6 +43,48 @@ const RootNavbar = () => {
           />
           <h1>Tourvisto</h1>
         </Link>
+
+        {user.status === "admin" ? (
+          <button
+            className="cursor-pointer px-8 py-3 text-lg font-semibold rounded-xl   shadow-lg hover:shadow-xl transition-all duration-300 hover:scale-105 transform"
+            onClick={() => {
+              setIsLoading(true);
+              navigate("/dashboard");
+            }}
+          >
+            {isLoading ? "navigating..." : "Dashboard"}
+          </button>
+        ) : (
+          <button
+            className="cursor-pointer px-8 py-3 text-lg font-semibold rounded-xl  shadow-lg hover:shadow-xl transition-all duration-300 hover:scale-105 transform"
+            disabled={isLoading}
+            onClick={() => {
+              setIsLoading(true);
+              navigate("/my-trips");
+            }}
+          >
+            {isLoading ? "navigating..." : "My Trips"}
+          </button>
+        )}
+
+        <button
+          className="cursor-pointer px-8 py-3 text-lg font-semibold rounded-xl  shadow-lg hover:shadow-xl transition-all duration-300 hover:scale-105 transform"
+          onClick={() => {
+            setIsLoading2(true);
+            navigate("/trips");
+          }}
+        >
+          {isLoading2 ? "Loading..." : "All Trips"}
+        </button>
+        <button
+          className="cursor-pointer px-8 py-3 text-lg font-semibold rounded-xl  shadow-lg hover:shadow-xl transition-all duration-300 hover:scale-105 transform"
+          onClick={() => {
+            setIsLoading3(true);
+            navigate("/trips/create");
+          }}
+        >
+          {isLoading3 ? "Loading..." : "Create a trip with AI"}
+        </button>
 
         <aside>
           {user.status === "admin" && (
@@ -66,7 +113,18 @@ const RootNavbar = () => {
           </button>
         </aside>
       </header>
+      <div className="p-5 root-nav wrapper  relative h-screen w-full bg-[url('/assets/images/hero-img.png')] bg-cover bg-center bg-no-repeat bg-fixed rounded-2xl">
+        <div className=" p-8 absolute inset-0 bg-black/50 flex flex-row items-center justify-between rounded-2xl"></div>
+      </div>
     </nav>
   );
 };
 export default RootNavbar;
+
+// a generic button if use;
+// // <button
+//             className="cursor-pointer px-8 py-3 text-lg font-semibold rounded-xl bg-gradient-to-r from-amber-800 to-orange-700 text-white shadow-lg hover:shadow-xl transition-all duration-300 hover:scale-105 transform"
+//             onClick={() => navigate("/my-trips")}
+//           >
+//             My Trips
+//           // </button>
