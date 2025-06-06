ecda905cecd53face10ffa2633cb02976f459186
suryaNoble <suryamani11121@gmail.com>
2025-06-06 18:49:52 +0530

    final commit for deployment

--- app/routes.ts
+++ app/routes.ts
@@ -8,6 +8,7 @@ import {
 export default [
   route("sign-in", "routes/root/sign-in.tsx"),
   route("api/create-trip", "routes/api/create-trip.ts"),
+
   layout("routes/admin/admin-layout.tsx", [
     route("dashboard", "routes/admin/dashboard.tsx"),
     route("all-users", "routes/admin/all-users.tsx"),
@@ -15,6 +16,7 @@ export default [
     route("trips/create", "routes/admin/create-trip.tsx"),
     route("trips/:tripId", "routes/admin/trip-detail.tsx"),
   ]),
+
   layout("routes/root/home-layout.tsx", [
     index("routes/root/HomePage.tsx"),
     route("/travel/:tripId", "routes/root/travel-detail.tsx"),
